323179fd2fecb27b04a27cf8656a51f057f8cadc
tschut <tschut@gmail.com>
2014-06-10 20:52:21 +0200

    Buildfix

--- railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/IRailCloud.java
+++ railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/IRailCloud.java
@@ -1,7 +1,11 @@
 package nl.loxia.raildocs.raildocs;
 
+import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
 /**
  * Created by Tiemen on 10-6-2014.
  */
+@Rest(rootUrl = "https://www.railcloud.nl/infra/", converters = { GsonHttpMessageConverter.class })
 public interface IRailCloud {
 }
