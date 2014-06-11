2951352cc9a8c2d0edcf725b69e5d4692cdf19d6
tschut <tschut@gmail.com>
2014-06-11 20:00:09 +0200

    Create PostListActivity

--- railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/IRailCloud.java
+++ railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/IRailCloud.java
@@ -5,6 +5,8 @@ import org.androidannotations.annotations.rest.RequiresAuthentication;
 import org.androidannotations.annotations.rest.Rest;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 
+import java.util.List;
+
 /**
  * Created by Tiemen on 10-6-2014.
  */
@@ -13,6 +15,10 @@ public interface IRailCloud {
     @Get("/namespaces")
     @RequiresAuthentication
     void getNamespaces();
+
+    @Get("/lijst/velden?type=nl.loxia.document.blad&veld=vlpost")
+    @RequiresAuthentication
+    List<String> getPosten();
 
     void setHttpBasicAuth(String username, String password);
 }
