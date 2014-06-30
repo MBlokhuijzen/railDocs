d5580b3991be5fb270da530df6963273e735d9f9
tschut <tschut@gmail.com>
2014-06-30 21:38:55 +0200

    Add download button to BladView

--- railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/BladView.java
+++ railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/BladView.java
@@ -1,30 +1,61 @@
 package nl.loxia.raildocs.raildocs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
+import org.apache.http.protocol.HTTP;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import nl.loxia.raildocs.raildocs.domein.Blad;
+import nl.loxia.raildocs.raildocs.util.CredentialsStore;
 
 /**
  * Created by Tiemen on 26-6-2014.
  */
 @EViewGroup(R.layout.list_item_blad)
 public class BladView extends RelativeLayout {
+    private final Context context;
     @ViewById
     protected TextView idnummer;
+
+    @Bean
+    protected CredentialsStore credentialsStore;
 
     private Blad blad;
 
     public BladView(Context context) {
         super(context);
+        this.context = context;
     }
 
     public void bind(Blad blad) {
         this.blad = blad;
         idnummer.setText(blad.idnummer);
+    }
+
+    @Click
+    protected void downloadButton() {
+        String uri = "https://" + silentEncode(credentialsStore.username) + ":" + silentEncode(credentialsStore.password) + "@" + blad.metaInfo.relaties.get("pdfBlad").get(0).url.replaceFirst("https://", "");
+        Intent openIntent = new Intent(Intent.ACTION_VIEW);
+        openIntent.setData(Uri.parse(uri));
+        context.startActivity(openIntent);
+    }
+
+    private String silentEncode(String in) {
+        try {
+            return URLEncoder.encode(in, HTTP.UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
