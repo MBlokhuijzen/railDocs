edb6611337ebb3647705b490a76a9c211a536631
tschut <tschut@gmail.com>
2014-07-03 19:51:25 +0200

    Implementing the navdrawer

--- railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/NavBarActivity.java
+++ railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/NavBarActivity.java
@@ -7,6 +7,9 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -20,30 +23,30 @@ public class NavBarActivity extends Activity {
     private ActionBarDrawerToggle drawerToggle;
     @ViewById
     protected DrawerLayout drawerLayout;
+    @ViewById
+    protected ListView drawer;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        drawer.setItemChecked(position, true);
+        drawerLayout.closeDrawer(drawer);
+    }
 
     @AfterViews
     protected void init() {
-        getActionBar().setDisplayShowHomeEnabled(true);
+        String[] titles = getResources().getStringArray(R.array.navdrawer_items);
+        drawer.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles));
+        drawer.setItemChecked(0, true);
 
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-            private CharSequence title = getTitle();
-            private CharSequence subtitle = getActionBar().getSubtitle();
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getActionBar().setTitle(title);
-                getActionBar().setSubtitle(subtitle);
-            }
+        drawer.setOnItemClickListener(new DrawerItemClickListener());
 
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                title = getTitle();
-                subtitle = getActionBar().getSubtitle();
-                getActionBar().setTitle(R.string.drawerOpenTitle);
-                if (subtitle != null) {
-                    getActionBar().setSubtitle("");
-                }
-            }
-        };
+        drawerToggle = new MyActionBarDrawerToggle();
 
         drawerToggle.syncState();
         drawerLayout.setDrawerListener(drawerToggle);
@@ -71,5 +74,30 @@ public class NavBarActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MyActionBarDrawerToggle extends ActionBarDrawerToggle {
+        private CharSequence title = getTitle();
+        private CharSequence subtitle = getActionBar().getSubtitle();
+
+        public MyActionBarDrawerToggle() {
+            super(NavBarActivity.this, NavBarActivity.this.drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
+        }
+
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            getActionBar().setTitle(title);
+            getActionBar().setSubtitle(subtitle);
+        }
+
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            title = getTitle();
+            subtitle = getActionBar().getSubtitle();
+            getActionBar().setTitle(R.string.drawerOpenTitle);
+            if (subtitle != null) {
+                getActionBar().setSubtitle("");
+            }
+        }
     }
 }
