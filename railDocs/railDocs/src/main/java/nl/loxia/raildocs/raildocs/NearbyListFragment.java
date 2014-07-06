63a2fff8a051677fcb35f2d8bb3e26e62dfb9faf
tschut <tschut@gmail.com>
2014-07-06 20:51:09 +0200

    Resolving location in the NearbyListFragment

--- railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/NearbyListFragment.java
+++ railDocs/railDocs/src/main/java/nl/loxia/raildocs/raildocs/NearbyListFragment.java
@@ -1,8 +1,19 @@
 package nl.loxia.raildocs.raildocs;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
@@ -11,10 +22,10 @@ import org.springframework.web.client.RestClientException;
 
 import java.util.List;
 
-import nl.loxia.raildocs.raildocs.util.BundleKeys;
-
 @EFragment(R.layout.fragment_post)
-public class NearbyListFragment extends BrowseListFragment implements AbsListView.OnItemClickListener {
+public class NearbyListFragment extends BrowseListFragment implements AbsListView.OnItemClickListener, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationClient locationClient;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -23,10 +34,25 @@ public class NearbyListFragment extends BrowseListFragment implements AbsListVie
     public NearbyListFragment() {
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        locationClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        locationClient.disconnect();
+    }
+
     @AfterViews
     public void init() {
         getActivity().setTitle(R.string.title_activity_nearby);
         listView.setOnItemClickListener(this);
+
+        locationClient = new LocationClient(getActivity(), this, this);
+
 //        loadData(getArguments().getString(BundleKeys.POST), getArguments().getString(BundleKeys.DOSSIER));
     }
 
@@ -50,5 +76,21 @@ public class NearbyListFragment extends BrowseListFragment implements AbsListVie
         } catch (RestClientException e) {
             loadingError();
         }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location location = locationClient.getLastLocation();
+        Toast.makeText(getActivity(), "Location: " + location.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
